c3ab2087efa8d464d7d61919f86522de8f7e4904
Kaviya <mkaviyamadeshvaran67@gmail.com>
2025-01-04 22:42:46 +0530

    frontend vercel json update

--- frontend/src/components/common/AxiosInstance.jsx
+++ frontend/src/components/common/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://edutechpro-backend.onrender.com', 
+  baseURL: 'http://localhost:5000', 
 });
 
 export default axiosInstance;
