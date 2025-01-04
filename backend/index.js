f9e5cd7f5f627332c92b08613e808f8908b54ff5
Kaviya <156312927+Kaviyamadeshvaran@users.noreply.github.com>
2025-01-04 19:27:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://edutechpro-onlinecourse-frontend.onrender.com",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
