7fc3f66069596e9210603d7d11ddf35028c70b4c
Kaviya <156312927+Kaviyamadeshvaran@users.noreply.github.com>
2025-01-05 08:19:48 +0530

    Update backend url from render

--- frontend/src/components/common/AxiosInstance.jsx
+++ frontend/src/components/common/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "https://edutechpro-onlinecourse-backend.onrender.com", 
+  baseURL: 'https://edutechpro-onlinecourse-backend.onrender.com', // Backend Render URL
 });
 
 export default axiosInstance;
